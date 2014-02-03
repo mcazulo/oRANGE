05fa47e53306b49f94d41d263e60498cae02a05f
mlcazulo <mlcazulo@gmail.com>
2014-02-03 16:33:25 -0500

    This is a test.

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -16,7 +16,8 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Team Orange!");
+        System.out.println("Hello Team Orange!/n Just trying out different things");
+        
     }
     
 }
